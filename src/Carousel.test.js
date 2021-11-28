c1723c911e3e841ae036b3906fe6032f152768b9
Jenny Ng <wcjennyng@gmail.com>
2021-11-28 14:18:49 -0500

    state carousel exercise

---
+++ src/Carousel.test.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Carousel from "./Carousel";
+
+it('renders without crashing', function() {
+  render(<Carousel />)
+})
+
+it('matches snapshot', function () {
+  const { asFragment } = render(<Carousel />)
+  expect(asFragment()).toMatchSnapshot()
+})
+
+it("works when you click on the right arrow", function() {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+  // expect the first image to show, but not the second
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the first
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+});
+
+//Test that fixes arrow bug
+it('works when you click on the left arrow', function () {
+  const { getByTestId, queryByAltText } = render(<Carousel />)
+  const leftArrow = getByTestId("left-arrow")
+  const rightArrow = getByTestId("right-arrow")
+
+  //move to the right
+  fireEvent.click(rightArrow)
+
+  //move back to the left, should show Image 1
+  fireEvent.click(leftArrow)
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+})
+
+it('hides and shows arrows on carousel', function () {
+  const { getByTestId } = render(<Carousel />)
+  const leftArrow = getByTestId("left-arrow")
+  const rightArrow = getByTestId("right-arrow")
+
+  //only right arrow shown - on first image
+  expect(leftArrow).toHaveClass("hidden")
+  expect(rightArrow).not.toHaveClass("hidden")
+
+  //move forward - both arrows should be shown
+  fireEvent.click(getByTestId("right-arrow"))
+
+  //only left arrow shown - on last image
+  fireEvent.click(rightArrow)
+  expect(leftArrow).not.toHaveClass("hidden")
+  expect(rightArrow).toHaveClass("hidden")
+})
